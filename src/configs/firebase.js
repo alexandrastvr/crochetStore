61b2bffdac5170ba59d115df956382450739e533
alexandrastvr <97674031+alexandrastvr@users.noreply.github.com>
2022-05-02 13:55:42 +0300

    with-config

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8tHrf-ObIdnHVhyS1w3Y_JfwFQDdhfQ0",
+    authDomain: "crochetstore-a2856.firebaseapp.com",
+    projectId: "crochetstore-a2856",
+    storageBucket: "crochetstore-a2856.appspot.com",
+    messagingSenderId: "280272374344",
+    appId: "1:280272374344:web:7625e21916cba7d32c667d"
+  };
+
+  export default firebaseConfig;
